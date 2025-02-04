1d7f8bd8d7d9cab2c31a63f6c23dafd16348bf02
GabrielVictorino8266 <gabriel.victorino2004@gmail.com>
2025-02-03 23:34:39 -0300

    created basic api example, listed all recipes

--- projeto_ferias/api/views.py
+++ projeto_ferias/api/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from django.http import JsonResponse
+from .mongodb import get_all_recipes
+
+def RecipeListView(request):
+    # Obter todas as receitas
+    recipes = get_all_recipes()
+    
+    # Retornar as receitas como uma resposta JSON
+    return JsonResponse({"recipes": recipes})
 
 # Create your views here.
