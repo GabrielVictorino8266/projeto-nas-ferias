dd4bfc26749e6c55b6050e6f501a3e78ff0bcadf
JoaoVallim2002 <joaovallim.vitor@gmail.com>
2025-02-05 19:49:03 -0300

    porta

--- API/manage.py
+++ API/manage.py
@@ -2,10 +2,10 @@
 import os
 import sys
 
-
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'projeto_ferias.settings')
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -14,8 +14,10 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
-    execute_from_command_line(sys.argv)
 
+    # Obtenha a variável de ambiente PORT
+    port = os.getenv("PORT", "8000")  # Se não estiver definida, usa 8000 como padrão
+    execute_from_command_line(["manage.py", "runserver", f"0.0.0.0:{port}"])
 
 if __name__ == '__main__':
     main()
